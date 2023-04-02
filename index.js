9cab556d3b6a4226d0aeca70bce5c58a873c9141
Nitin Gupta <nitingupta1906@gmail.com>
2023-04-02 19:39:44 +0530

    port Change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
